e6ef3834d7abf103683aa718e6ed73e77d8adc2f
Ezekiel Kigbo <theatlasroom@gmail.com>
2017-02-15 09:32:37 +1100

    wip - file open result

--- src/bin/file_io.rs
+++ src/bin/file_io.rs
@@ -1,12 +1,30 @@
-// basic file IO operations 
-fn write_file(&file, data){
-}
+// basic file IO operations
+use std::fs;
+use std::env;
 
-fn read_file(&file){
-}
 
-fn open_file(){
-}
+// fn write_file(&file, data){
+// }
+//
+// fn read_file(&file){
+// }
+//
+// fn open_file(){
+// }
 
 fn main(){
+
+    // We assume that we are in a valid directory.
+    let p = env::current_dir().unwrap();
+    println!("The current directory is {}", p.display());
+
+    let f = "./data/text-file.txt";
+    let metadata = fs::metadata(f).unwrap();
+    println!("Is {:?} a valid file: {:?}", f, metadata.is_file());
+
+    // open the file for reading
+    let mut handle = match fs::File::open(f){
+        Some(handle) => println!("File\n{:?}", handle),
+        Err => println!("Error\n{:?}", handle),
+    };
 }
