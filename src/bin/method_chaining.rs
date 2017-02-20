68b1fa4c00d90203dd28bc3b2451e1b25370aa6d
Ezekiel Kigbo <theatlasroom@gmail.com>
2017-02-20 21:17:08 +1100

    updated the method chaining example to use the builder pattern

--- src/bin/method_chaining.rs
+++ src/bin/method_chaining.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let r: Rect = Rect { w: 10.0, h: 10.0 };
+    let r: Rect = RectBuilder::new()
+                    .width(10.0)
+                    .height(10.0)
+                    .finalize();
+
     let p = r.scale(2.0)
      .scale(0.25)
      .scale(5.0)
@@ -11,10 +15,18 @@ fn main() {
     println!("Rectangle w: {:?} h: {:?} area: {:?}", p.w, p.h, p.area());
 }
 
+// Rect struct
 struct Rect {
     w: f64,
     h: f64,
 }
+
+// Define a builder struct, this can be used to construct our Rects
+struct RectBuilder {
+    w: f64,
+    h: f64
+}
+
 
 impl Rect {
     fn scale(&self, factor: f64) -> Rect {
@@ -23,5 +35,27 @@ impl Rect {
 
     fn area(&self) -> f64 {
         self.w * self.h
+    }
+}
+
+impl RectBuilder {
+    fn new() -> RectBuilder {
+        RectBuilder { w: 0.0, h: 0.0 }
+    }
+
+    // Each function for the builder returns a mutable reference to the builder we are using
+    fn width(&mut self, w: f64) -> &mut RectBuilder {
+        self.w = w;
+        self
+    }
+
+    fn height(&mut self, h: f64) -> &mut RectBuilder {
+        self.h = h;
+        self
+    }
+
+    // the finalize method returns the type we have constructed
+    fn finalize(&mut self) -> Rect {
+        Rect {w: self.w, h: self.h }
     }
 }
