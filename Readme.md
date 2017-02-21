450cf8460720313f5dc511d8c58187eaacccd4ed
Ezekiel <hi@eakigbo.me>
2017-02-22 01:19:30 +1100

    updated readme information and links added rand crate finished your_dj_name

--- Readme.md
+++ Readme.md
@@ -16,3 +16,10 @@ ie  cargo run --bin hello_world`
 * [Elapsed time](./src/bin/elapsed_time.rs)
 * [Dice rolling](./src/bin/dice_roll.rs)
 * [Palindrome](./src/bin/palindrome.rs)
+
+## Data structures
+* [Your DJ Name](./src/bin/your_dj_name.rs)
+* [Naive 2D Matrix](./src/bin/matrix_math.rs)
+* [Method chaining](./src/bin/method_chaining.rs)
+
+## I/O
--- src/bin/your_dj_name.rs
+++ src/bin/your_dj_name.rs
@@ -1,23 +1,37 @@
 // return your dj name
 // take random strings and mash them togethor
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
 
 fn main() {
-    let names = ["James", "Joan", "Willow", "Darren", "Phil", "Ashley", "Kate", "Carla"];
-    let genres = ["Blues", "Rock", "Pop", "Jazz", "Funk", "Soul", "Rap"];
-    let dj_tuple: &(String, String, String) = dj_name_generator(&names, &genres);
-    let dj_name = tuple_to_string(&dj_tuple);
+    let names = &["James", "Joan", "Willow", "Darren", "Phil", "Ashley", "Kate", "Carla"];
+    let genres = &["Blues", "Rock", "Pop", "Jazz", "Funk", "Soul", "Rap"];
+    let dj_tuple: (String, String, String) = dj_name_generator(names, genres);
+    let dj_name = tuple_to_string(dj_tuple);
     println!("Your DJ name is: {:?}", dj_name);
 }
 
-fn dj_name_generator(names: &[str], genres: &[str]) -> (str, str, str) {
-    let name = names[0];
-    let genre = genres[0];
+fn dj_name_generator(names: &[&str], genres: &[&str]) -> (String, String, String) {
+    let name_index = random(0, names.len());
+    let genre_index = random(0, genres.len());
+    let name = names[name_index];
+    let genre = genres[genre_index];
     let res = ("DJ".to_string(), name.to_string(), genre.to_string());
-    &res;
+    res
 }
 
-fn tuple_to_string(tup: (&str, &str, &str)) -> str {
-    let postfix: String = ", the great";
-    let result: &str = tup.0 + tup.1 + tup.2 + &postfix;
-    &result;
+fn tuple_to_string(tup: (String, String, String)) -> String {
+    let postfix: String = ", the great".to_string();
+    let result: String = tup.0 + " " + &tup.1 + " " + &tup.2 + &postfix;
+    result
+}
+
+// TODO: this should be in a seperate module for utils
+// returns a random number within a range specified
+fn random(min: usize, max: usize) -> usize {
+    let between = Range::new(min, max);
+    let mut rng = rand::thread_rng();
+    let number = between.ind_sample(&mut rng);
+    println!("random {:?}", number);
+    number as usize
 }
